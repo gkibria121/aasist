01d8bc2d5dc7ed4318caf126a813aea1b8c1bfbf
gkibria121 <gkibria121@gmail.com>
2025-12-05 09:00:41 +0600

    add models

---
+++ models/SimpleCNN.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleCNN(nn.Module):
+    def __init__(self, num_classes=10, input_channels=3):
+        """
+        Simple CNN for image classification
+        
+        Args:
+            num_classes: Number of output classes
+            input_channels: Number of input channels (3 for RGB, 1 for grayscale)
+        """
+        super(SimpleCNN, self).__init__()
+        
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        
+        # Max pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Dropout for regularization
+        self.dropout = nn.Dropout(0.5)
+        
+        # Fully connected layers
+        # Assuming input image size is 32x32 (like CIFAR-10)
+        # After 3 pooling layers: 32->16->8->4
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+        
+    def forward(self, x):
+        # Conv block 1
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        
+        # Conv block 2
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        
+        # Conv block 3
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        
+        # Flatten
+        x = x.view(x.size(0), -1)
+        
+        # Fully connected layers
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        
+        return x
